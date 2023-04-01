bcf0ee0aadaea8a60d06340c8226569a34fd570b
Aditya Bhatt <adityabhatt789@gmail.com>
2023-04-01 14:24:09 +0530

    completed project-1

---
+++ jsx/src/App.js
@@ -0,0 +1,51 @@
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+import "bulma/css/bulma.css";
+
+function App() {
+   return (
+      <div>
+      <section className="hero is-warning">
+        <div className="hero-body">
+          <p className="title">
+          Personal Digital Assistants
+          </p>
+        </div>
+      </section>
+         <div className="container">
+            <section className="section">
+               <div className="columns">
+                  <div className="column is-3">
+                     <ProfileCard
+                        title="Alexa"
+                        handle="@alexa98"
+                        image={AlexaImage}
+                        description="Alexa was created by Amazon"
+                     />
+                  </div>
+                  <div className="column is-3">
+                     <ProfileCard
+                        title="Cortana"
+                        handle="@cortananice"
+                        image={CortanaImage}
+                        description="Cortana was created by Microsoft"
+                     />
+                  </div>
+                  <div className="column is-3">
+                     <ProfileCard
+                        title="Siri"
+                        handle="@siri01"
+                        image={SiriImage}
+                        description="Siri was created by Apple"
+                     />
+                  </div>
+               </div>
+            </section>
+         </div>
+      </div>
+   );
+}
+
+export default App;
