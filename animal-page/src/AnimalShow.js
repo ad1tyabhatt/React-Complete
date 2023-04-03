15adcbc52f81f43d257f90de51b1e38bada2fdaf
Aditya Bhatt <adityabhatt789@gmail.com>
2023-04-03 12:30:57 +0530

    added images

--- animal-page/src/AnimalShow.js
+++ animal-page/src/AnimalShow.js
@@ -1,9 +1,32 @@
-function AnimalShow(){
-  return (
-    <div>
-      COW!
-    </div>
-  )
+import { useState } from 'react';
+import bird from './img/bird.svg';
+import cat from './img/cat.svg';
+import cow from './img/cow.svg';
+import dog from './img/dog.svg';
+import gator from './img/gator.svg';
+import heart from './img/heart.svg';
+import horse from './img/horse.svg';
+
+const svgMap = {
+  bird, // similar to bird:bird
+  cat,
+  cow,
+  dog,
+  gator,
+  horse
+}
+
+function AnimalShow({ type }) {
+  const [clicks,setClicks] = useState(0)
+
+  const handleClick = () =>{
+    setClicks(clicks+1)
+  }
+   return <div onClick={handleClick}>
+   <img src={svgMap[type]} alt="img" />
+   <img src={heart} alt="img heart" style={{width:10+10*clicks + 'px'
+   }} />
+   </div>;
 }
 
 export default AnimalShow;
--- animal-page/src/App.js
+++ animal-page/src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import AnimalShow from "./AnimalShow";
 
-function App(){
-  const handleClick = ()=>{
-    console.log("Button Clicked")
-  }
-  return(
-    <div>
-    <button>Add Animal</button>
-    </div>
-  )
+function getAnimal (){
+  const animals = ['bird','cat','cow','dog','gator','horse'];
+  return animals[Math.floor(Math.random()*animals.length)]
+}
+
+function App() {
+   const [animals,setAnimals] = useState([]);
+
+   const handleClick = () => {
+    setAnimals([...animals,getAnimal()])
+   };
+
+   const renderedAnimals = animals.map((animal,index)=>{
+     return <AnimalShow type={animal} key={index}/>
+   })
+   return (
+      <div>
+         <button onClick={handleClick}>Add Animal</button>
+         <div>{renderedAnimals}</div>
+      </div>
+   );
 }
 export default App;
