e6657fc799523b1ca69323e1ddc7f35730d31a6e
Aditya Bhatt <adityabhatt789@gmail.com>
2023-04-03 13:56:50 +0530

    completed Animal project

--- animal-page/src/AnimalShow.js
+++ animal-page/src/AnimalShow.js
@@ -1,33 +1,39 @@
-import { useState } from 'react';
-import './AnimalShow.css';
-import bird from './img/bird.svg';
-import cat from './img/cat.svg';
-import cow from './img/cow.svg';
-import dog from './img/dog.svg';
-import gator from './img/gator.svg';
-import heart from './img/heart.svg';
-import horse from './img/horse.svg';
+import { useState } from "react";
+import "./AnimalShow.css";
+import bird from "./img/bird.svg";
+import cat from "./img/cat.svg";
+import cow from "./img/cow.svg";
+import dog from "./img/dog.svg";
+import gator from "./img/gator.svg";
+import heart from "./img/heart.svg";
+import horse from "./img/horse.svg";
 
 const svgMap = {
-  bird, // similar to bird:bird
-  cat,
-  cow,
-  dog,
-  gator,
-  horse
-}
+   bird, // similar to bird:bird
+   cat,
+   cow,
+   dog,
+   gator,
+   horse,
+};
 
 function AnimalShow({ type }) {
-  const [clicks,setClicks] = useState(0)
+   const [clicks, setClicks] = useState(0);
 
-  const handleClick = () =>{
-    setClicks(clicks+1)
-  }
-   return <div className='animal-show' onClick={handleClick}>
-   <img className='animal' src={svgMap[type]} alt="img" />
-   <img className='heart' src={heart} alt="img heart" style={{width:10+10*clicks + 'px'
-   }} />
-   </div>;
+   const handleClick = () => {
+      setClicks(clicks + 1);
+   };
+   return (
+      <div className="animal-show" onClick={handleClick}>
+         <img className="animal" src={svgMap[type]} alt="img" />
+         <img
+            className="heart"
+            src={heart}
+            alt="img heart"
+            style={{ width: 10 + 10 * clicks + "px" }}
+         />
+      </div>
+   );
 }
 
 export default AnimalShow;
--- animal-page/src/App.js
+++ animal-page/src/App.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import AnimalShow from "./AnimalShow";
-import './App.css';
+import "./App.css";
 
-function getAnimal (){
-  const animals = ['bird','cat','cow','dog','gator','horse'];
-  return animals[Math.floor(Math.random()*animals.length)]
+function getAnimal() {
+   const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+   return animals[Math.floor(Math.random() * animals.length)];
 }
 
 function App() {
-   const [animals,setAnimals] = useState([]);
+   const [animals, setAnimals] = useState([]);
 
    const handleClick = () => {
-    setAnimals([...animals,getAnimal()])
+      setAnimals([...animals, getAnimal()]);
    };
 
-   const renderedAnimals = animals.map((animal,index)=>{
-     return <AnimalShow type={animal} key={index}/>
-   })
+   const renderedAnimals = animals.map((animal, index) => {
+      return <AnimalShow type={animal} key={index} />;
+   });
    return (
       <div className="app">
          <button onClick={handleClick}>Add Animal</button>
